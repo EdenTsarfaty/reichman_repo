8741ace3c4ee23771a5a36b2f853feae2fc7829e
Eden <eden.tsarfaty@gmail.com>
2023-12-23 17:00:12 +0200

    First HW assignment exercises

---
+++ 1/AddTwo/AddTwo.java
@@ -0,0 +1,10 @@
+public class AddTwo {
+	public static void main(String[] args) {
+		/*Recieves two integers as input, adds them up, and prints the
+		exercise in a readable format*/
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println(a + " + " + b + " = " + (a+b));
+
+	}
+}
---
+++ 1/Coins/Coins.java
@@ -0,0 +1,7 @@
+public class Coins {
+	public static void main(String[] args) {
+		//Solves a linear equation in the form of a*x+b=c and displays the solution
+		int a = Integer.parseInt(args[0]);
+		int a = Integer.parseInt(args[0]);
+		
+}
